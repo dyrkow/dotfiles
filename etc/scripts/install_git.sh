3078486849d05ea997597cc45d5bbe39113fda48
WebWorkDeveloper <webworkdeveloper@gmail.com>
2022-03-08 19:18:00 +0300

    Add git installation scripts

---
+++ etc/scripts/install_git.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git --version > /dev/null
+
+if [ $? -eq 0 ]
+then
+    echo "Git installed!"
+else
+    echo "Installing git..."
+    /bin/bash -c "$(brew install git)"
+    if [ $? -ne 0 ]
+    then
+        echo: "Git not installed"
+        exit 1
+    else
+        echo "Git installation finished!"
+    fi
+fi
