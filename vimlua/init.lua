334032291993faeaa76c4492ac396e67fbe34f7b
dyrkow <webworkdeveloper@gmail.com>
2024-02-07 22:29:28 +0300

    Add lazy package manager and first package neotree

--- vimlua/init.lua
+++ vimlua/init.lua
@@ -1,8 +1,8 @@
-local opt = vim.opt
-local g = vim.g
+local keymap = require("keymap");
 
--- TODO: самый популярный и поддерживаемый пакетный менеджер
--- https://github.com/folke/lazy.nvim
+local opt = vim.opt
+local api = vim.api
+local g = vim.g
 
 -- Кодировка файла
 opt.encoding = "UTF-8"
@@ -81,3 +81,34 @@ opt.spell = true
 
 -- Лидеркей
 g.mapleader = " "
+
+-- Самый популярный и поддерживаемый пакетный менеджер
+-- https://github.com/folke/lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
+})
+
+keymap.nmap('<leader>n', '<cmd>:Neotree toggle<CR>')
