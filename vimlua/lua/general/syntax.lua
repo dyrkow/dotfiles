677a4b4d0967cc5a5945f0b0fa1a9547ec2df50b
dyrkow <webworkdeveloper@gmail.com>
2024-02-08 23:15:40 +0300

    Add color schema in lua vim

--- vimlua/lua/general/syntax.lua
+++ vimlua/lua/general/syntax.lua
@@ -1,5 +1,11 @@
 --[[ Конфигурация синтаксиса ]]--
 
+
+-- Настройка цветовой схемы
+vim.cmd("set background=dark")
+vim.cmd("syntax on")
+vim.cmd("colorscheme hemisu")
+
 -- Визуализация пробельных символов
 vim.opt.listchars:append {
   tab = "> ",
