ba632041716e1410679c948f07465ad0ea62a2c3
dyrkow <webworkdeveloper@gmail.com>
2024-02-18 22:44:16 +0300

    Update font

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -2,9 +2,6 @@
 
 MAC_FONT_LIBRARY="/Library/Fonts"
 
-cp "./fonts/MesloLGS NF Bold Italic.ttf" $MAC_FONT_LIBRARY
-cp "./fonts/MesloLGS NF Bold.ttf" $MAC_FONT_LIBRARY
-cp "./fonts/MesloLGS NF Italic.ttf" $MAC_FONT_LIBRARY
-cp "./fonts/MesloLGS NF Regular.ttf" $MAC_FONT_LIBRARY
+cp -r ./fonts/* $MAC_FONT_LIBRARY
 
 exit 0
