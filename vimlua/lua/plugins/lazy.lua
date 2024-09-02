d7079b653b9611244cd8f93fec1144db6252e95c
dyrkow <webworkdeveloper@gmail.com>
2024-09-02 21:04:51 +0300

    Enable treeseeter

--- vimlua/lua/plugins/lazy.lua
+++ vimlua/lua/plugins/lazy.lua
@@ -37,10 +37,10 @@ require("lazy").setup({
      "nvim-telescope/telescope.nvim", tag = "0.1.6",
      dependencies = { "nvim-lua/plenary.nvim" }
   },
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   build = ":TSUpdate",
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
 })
 
 require('plugins/ibl')
@@ -50,7 +50,7 @@ require('plugins/neoscroll')
 require('plugins/hightlight')
 require('plugins/autopairs')
 require('plugins/surround')
--- require('plugins/treesitter')
+require('plugins/treesitter')
 require('plugins/tmuxnavigation')
 require('plugins/comment')
 require('plugins/telescope')
