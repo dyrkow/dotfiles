177d874ef7e593bf259174a72c3dabf9f1ed94bc
dyrkow <webworkdeveloper@gmail.com>
2024-09-15 21:59:16 +0300

    Comment for install vim on empty device

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -9,6 +9,11 @@ else
     echo "[Nvim] installing..."
     /bin/bash -c "$(brew install neovim)"
 
+    # 1. Устанавливает Plug плагин для загрузки плагинов
+    # sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    #    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    # 2. После его установки нужно вызывать cvim чтобы поставить конфигурацию nvim
+
     if [ $? -ne 0 ]
     then
         echo: "[Nvim] - installation failed!"
