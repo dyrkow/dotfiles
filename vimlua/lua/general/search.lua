cde242e3af383d8395aba0cd151100bb224972f8
dyrkow <webworkdeveloper@gmail.com>
2024-09-17 21:58:52 +0300

    Fix serch hightligth

--- vimlua/lua/general/search.lua
+++ vimlua/lua/general/search.lua
@@ -5,5 +5,5 @@ local g = vim.g
 
 opt.ignorecase = true -- Игнорирует регистр при поиске в файле
 opt.smartcase = true -- Игнорирует mixed кейсы при поиске в файле
-vim.api.nvim_set_option('hlsearch', true)
+vim.api.nvim_set_option('hlsearch', false)
 
