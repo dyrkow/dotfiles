e1f9e42afadf707d3079be6b1d9aa249979d1b5b
dyrkow <webworkdeveloper@gmail.com>
2024-10-24 23:00:51 +0300

    Add border support for aerospace

--- aerospace/install.sh
+++ aerospace/install.sh
@@ -1,2 +1,7 @@
 brew install --cask nikitabobko/tap/aerospace
+
+# https://github.com/FelixKratz/JankyBorders?tab=readme-ov-file
+brew tap FelixKratz/formulae
+brew install borders
+
 cp "$(dirname "$0")/.aerospace.toml" ~/
