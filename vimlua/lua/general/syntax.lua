401ae9999110faede9ca50c214b159acf798ccb6
dyrkow <webworkdeveloper@gmail.com>
2025-09-10 21:19:47 +0300

    use hemisu

--- vimlua/lua/general/syntax.lua
+++ vimlua/lua/general/syntax.lua
@@ -5,6 +5,8 @@
 vim.cmd("set background=dark")
 vim.cmd("syntax on")
 vim.cmd.colorsche('hemisu')
+-- моя кастомная схема похожая на hemisu
+-- vim.cmd.colorsche('partizan')
 
 -- Визуализация пробельных символов
 vim.opt.listchars:append {
