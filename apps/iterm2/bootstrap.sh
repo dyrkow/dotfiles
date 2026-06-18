9de1d53c70051ba756ce60fc047f16e0f3e68e65
dyrkow <webworkdeveloper@gmail.com>
2026-06-18 21:47:52 +0300

    Add iterm2 settings

--- apps/iterm2/bootstrap.sh
+++ apps/iterm2/bootstrap.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 MODULE_NAME=iterm2
 INSTALL_CASK=iterm2
+APPLY_FILES=(
+    "com.googlecode.iterm2.plist:$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+)
 
 source "$(dirname "$0")/../../scripts/common.sh"
 run_module "$@"
