cc4cd962da582a075bdcc5951aab3c097fc5167a
dyrkow <webworkdeveloper@gmail.com>
2026-07-21 22:06:35 +0300

    Fix

--- apps/fliqlo/bootstrap.sh
+++ apps/fliqlo/bootstrap.sh
@@ -11,7 +11,7 @@ install() {
         return 0
     fi
 
-    if brew install --cask --no-quarantine fliqlo; then
+    if brew cask install --no-quarantine fliqlo; then
         echo "✅ installed ${MODULE_NAME}"
     else
         echo "❌ ERROR: installation failed" >&2
