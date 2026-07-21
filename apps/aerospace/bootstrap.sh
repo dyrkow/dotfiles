865805be0c6f8b74c5045347d4d4e6993439a306
dyrkow <webworkdeveloper@gmail.com>
2026-07-21 21:39:36 +0300

    Fix aerospace

--- apps/aerospace/bootstrap.sh
+++ apps/aerospace/bootstrap.sh
@@ -11,15 +11,11 @@ install() {
     if skip_if_command_exists "${MODULE_NAME}"; then
         return 0
     fi
-    if /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/nikitabobko/aerospace/main/install.sh)"; then
-        if command -v "${MODULE_NAME}" >/dev/null 2>&1; then
-            echo "✅ installed ${MODULE_NAME}"
-        else
-            echo "❌ ERROR: installation failed (binary not found after install)" >&2
-            return 1
-        fi
+
+    if brew install --cask nikitabobko/tap/aerospace; then
+        echo "✅ installed ${MODULE_NAME}"
     else
-        echo "❌ ERROR: installation script failed" >&2
+        echo "❌ ERROR: installation failed" >&2
         return 1
     fi
 }
