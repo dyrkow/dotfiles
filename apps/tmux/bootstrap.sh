4ce611797e87a7df590d740290ca782a098811d5
dyrkow <webworkdeveloper@gmail.com>
2026-07-24 23:02:25 +0300

    trye fix

--- apps/tmux/bootstrap.sh
+++ apps/tmux/bootstrap.sh
@@ -7,10 +7,15 @@ APPLY_FILES=(".tmux.conf:$HOME/.tmux.conf")
 
 source "$(dirname "$0")/../../scripts/common.sh"
 
-# Clone TPM repository if it doesn't exist
-if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-  info "Installing TPM (Tmux Plugin Manager)..."
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
+install() {
+    # Install tmux using homebrew
+    install_formula "${INSTALL_FORMULA}"
+
+    # Install TPM
+    if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+        echo "Installing TPM (Tmux Plugin Manager)..."
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    fi
+}
 
 run_module "$@"
