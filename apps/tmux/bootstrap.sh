86473190c0b7568e41a8ce3eb64944cd789c94aa
dyrkow <webworkdeveloper@gmail.com>
2026-07-24 22:59:03 +0300

    Add install tpm automatically

--- apps/tmux/bootstrap.sh
+++ apps/tmux/bootstrap.sh
@@ -6,4 +6,11 @@ INSTALL_FORMULA=tmux
 APPLY_FILES=(".tmux.conf:$HOME/.tmux.conf")
 
 source "$(dirname "$0")/../../scripts/common.sh"
+
+# Clone TPM repository if it doesn't exist
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  info "Installing TPM (Tmux Plugin Manager)..."
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
 run_module "$@"
