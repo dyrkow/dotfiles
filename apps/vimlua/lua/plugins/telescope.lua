67afb62593133de9e98965f211d18bd16ee77f62
dyrkow <webworkdeveloper@gmail.com>
2026-08-20 17:02:39 +0300

    Navigate by buffers by jk

--- apps/vimlua/lua/plugins/telescope.lua
+++ apps/vimlua/lua/plugins/telescope.lua
@@ -4,7 +4,13 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>b', function()
+  builtin.buffers({
+    sort_mru = true,
+    ignore_current_buffer = true,
+    initial_mode = "normal",
+  })
+end, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
 vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
