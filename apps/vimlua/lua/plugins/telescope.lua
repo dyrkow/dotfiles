2c424a30194c3ffb9ce4e64dd89c34748645e13b
dyrkow <webworkdeveloper@gmail.com>
2026-08-21 12:42:12 +0300

    Ignore dirs from grep

--- apps/vimlua/lua/plugins/telescope.lua
+++ apps/vimlua/lua/plugins/telescope.lua
@@ -2,7 +2,41 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+-- Папки, которые не нужно индексировать при поиске
+local exclude_args = {
+  "--glob", "!node_modules/",
+  "--glob", "!build/",
+  "--glob", "!dist/",
+  "--glob", "!out/",
+  "--glob", "!target/",
+  "--glob", "!coverage/",
+  "--glob", "!__pycache__/",
+  "--glob", "!.venv/",
+  "--glob", "!.next/",
+}
+
+require('telescope').setup({
+  defaults = {
+    -- Аргументы ripgrep для live_grep и grep_string
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      unpack(exclude_args),
+    },
+  },
+})
+
+vim.keymap.set('n', '<leader>ff', function()
+  builtin.find_files({
+    find_command = { "rg", "--files", "--color", "never", unpack(exclude_args) },
+  })
+end, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', function()
   builtin.buffers({
