38e48c1a4828f1e4b578fcb44f582880c0e59960
dyrkow <webworkdeveloper@gmail.com>
2026-09-04 10:21:09 +0300

    One diff for only one file

--- apps/vimlua/lua/plugins/diffview.lua
+++ apps/vimlua/lua/plugins/diffview.lua
@@ -2,7 +2,7 @@
 -- https://github.com/sindrets/diffview.nvim
 require('diffview').setup({})
 
-vim.keymap.set('n', '<leader>gd', '<cmd>DiffviewOpen<cr>')
+vim.keymap.set('n', '<leader>gd', '<cmd>DiffviewOpen -- %<cr>')
 vim.keymap.set('n', '<leader>gD', '<cmd>DiffviewClose<cr>')
 vim.keymap.set('n', '<leader>gf', '<cmd>DiffviewFileHistory %<cr>')
 vim.keymap.set('n', '<leader>gl', '<cmd>DiffviewFileHistory<cr>')
