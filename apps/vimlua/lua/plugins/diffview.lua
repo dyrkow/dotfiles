ca3f2c7503a1c4dd76be10146b4e482c7577da78
dyrkow <webworkdeveloper@gmail.com>
2026-09-15 16:02:11 +0300

    Add show diff between current branch and dev/main/master

--- apps/vimlua/lua/plugins/diffview.lua
+++ apps/vimlua/lua/plugins/diffview.lua
@@ -2,7 +2,27 @@
 -- https://github.com/sindrets/diffview.nvim
 require('diffview').setup({})
 
+local function system_ok_or_empty(cmd)
+  local result = vim.fn.system(cmd)
+  if vim.v.shell_error == 0 then
+    return result:gsub('%s+$', '')
+  end
+  return ''
+end
+
+local function default_branch()
+  for _, candidate in ipairs({ 'dev', 'main', 'master' }) do
+    if system_ok_or_empty('git rev-parse --verify --quiet refs/heads/' .. candidate) ~= '' then
+      return candidate
+    end
+  end
+  return 'dev'
+end
+
 vim.keymap.set('n', '<leader>gd', '<cmd>DiffviewOpen<cr>')
+vim.keymap.set('n', '<leader>gm', function()
+  vim.cmd('DiffviewOpen ' .. default_branch() .. '...HEAD')
+end)
 vim.keymap.set('n', '<leader>gD', '<cmd>DiffviewClose<cr>')
 vim.keymap.set('n', '<leader>gf', '<cmd>DiffviewFileHistory %<cr>')
 vim.keymap.set('n', '<leader>gl', '<cmd>DiffviewFileHistory<cr>')
